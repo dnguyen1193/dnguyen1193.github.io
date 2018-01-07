48011cab3f94ce4e632ca34252f38b2421809bd7
dnguyen <dnguyen@broadinstitute.org>
2018-01-07 17:07:35 -0500

    adding smooth scroll functionality on page load with anchor

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ $(document).ready(function() {
     $(document).on('scroll', onScroll);
     var navItems = $('.nav-item').add($('.navbar-brand'));
     navItems.on('click', function(e) {
+        e.preventDefault();
         var target = $($(this).attr('href')).parent();
         if (target.length) {
             $('html, body').animate({
@@ -33,4 +34,8 @@ $(document).ready(function() {
             });
         }
     });
+
+    if (window.location.hash) {
+        $('[href="' + location.hash + '"]').click();
+    }
 });
