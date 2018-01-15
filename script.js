31f3fa986670d1b52bfdf01b950cb6366d37bc7a
dnguyen <dnguyen@broadinstitute.org>
2018-01-15 12:50:05 -0500

    using bootstrap scrollspy instead of self coded one

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
-var onScroll = function() {
-    var navbarHeight = $('.navbar').height();
-    var scrollPosTop = $(document).scrollTop() + navbarHeight;
-    var scrollPosMid = scrollPosTop + window.innerHeight * 0.5;
-    var navItems = $('.nav-item');
-
-    navItems.each(function() {
-        var currItem = $(this);
-        var refElement = $(currItem.attr('href'));
-
-        if (!refElement.length) { return; }
-
-        var refElementPosTop = refElement.position().top;
-
-        if (scrollPosTop > navbarHeight && scrollPosTop <= refElementPosTop
-            && refElementPosTop <= scrollPosMid) {
-            $('.nav-item').removeClass('active');
-            currItem.addClass('active');
-        } else {
-            currItem.removeClass('active');
-        }
-    });
-};
-
 $(document).ready(function() {
-    $(document).on('scroll', onScroll);
+    $('body').scrollspy();
     var navItems = $('.nav-item').add($('.navbar-brand'));
     navItems.on('click', function(e) {
         e.preventDefault();
