1e9046189fafff9294185385cc695519ef277665
dnguyen <dnguyen@broadinstitute.org>
2018-01-15 12:57:20 -0500

    moving nav item click anonymous function out as separate named function for cleaner code

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+var navItemClickCallback = function(e) {
+    e.preventDefault();
+    var target = $($(this).attr('href')).parent();
+    if (target.length) {
+        $('html, body').animate({
+            scrollTop:target.offset().top - $('.navbar').outerHeight()
+        });
+        target.attr('tabindex', -1);
+        target.focus();
+    }
+};
+
 $(document).ready(function() {
     $('body').scrollspy();
     var navItems = $('.nav-item').add($('.navbar-brand'));
-    navItems.on('click', function(e) {
-        e.preventDefault();
-        var target = $($(this).attr('href')).parent();
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop:target.offset().top - $('.navbar').outerHeight()
-            });
-            target.attr('tabindex', -1);
-            target.focus();
-        }
-    });
+    navItems.on('click', navItemClickCallback);
 
     if (window.location.hash) {
         $('[href="' + location.hash + '"]').click();
